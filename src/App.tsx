c446857b1667dc174330ddc2cfe6d340ca754199
ghanshyamtanchak <ghtanchak77@gmail.com>
2022-11-27 18:43:07 +0530

    create ui for registration form

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
-function App() {
-  return <div className='App'>App</div>;
-}
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/login";
+import Register from "./pages/register";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
